4ed7d94b0ebbaddc5ce7fc42a6df07d6ff2d29d8
Ethan Walker <81318227+EthanWalkerGit@users.noreply.github.com>
2024-05-29 14:04:48 -0400

    added backend (mongodb)

---
+++ server/server.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const Album = require('./models/albumModel')
+const app = express()
+
+app.use(express.json())
+
+app.listen(3000, ()=> {
+  console.log("Node App is running on port 3000")
+})
+
+//get and show all items in db
+app.get('/albums', async(req, res) => {
+  try {
+    const albums = await Album.find({})
+    res.status(200).json(albums)
+  } catch {
+    res.status(500).json({message: error.message})
+  }
+})
+
+//get items by id
+app.get('/albums/:id', async(req, res) =>{
+  try {
+    const {id} = req.params;
+    const albums = await Album.findById(id);
+    res.status(200).json(albums)
+  } catch {
+    res.status(500).json({message: error.message})
+  }
+})
+
+//add items 
+app.post('/albums', async(req, res) => {
+  try {
+    const albums = await Album.create(req.body)
+    res.status(200).json(albums);
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+  }
+})
+
+//update items already in db
+app.put('/albums/:id', async(req, res) => {
+  try {
+    const {id} = req.params;
+    const album = await Album.findByIdAndUpdate(id, req.body);
+
+    if(!album){
+      return res.status(404).json({message: 'cannot find product with ID ${id}'})
+    }
+    const updatedAlbum = await Album.findById(id);
+    res.status(200).json({updatedProduct});
+
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+//delete entry from db
+
+app.delete('/albums/:id', async(req, res) => {
+  try {
+    const {id} = req.params;
+    const album = await Album.findByIdAndDelete(id);
+    if(!album){
+      return res.status(404).json({message: 'cannot find product with ID ${id}'})
+    }
+    res.status(200).json(album);
+
+  } catch {
+    res.status(500).json({message: error.message})
+  }
+})
+
+// connection
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => {
+  console.log('connected to MongoDB')
+})
+.catch((error) => {
+  console.log('error connecting to MongoDB:', error.message)
+});
