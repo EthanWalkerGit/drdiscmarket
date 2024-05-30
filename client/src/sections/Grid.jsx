806b0bb019112190fed463f855a651bf5accf9a3
Ethan Walker <81318227+EthanWalkerGit@users.noreply.github.com>
2024-05-30 08:58:13 -0400

    better grid

--- client/src/sections/Grid.jsx
+++ client/src/sections/Grid.jsx
@@ -17,17 +17,19 @@ function Grid() {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 grid-rows-3 h-full w-full">
       {albums.map(album => (
-        <div key={album._id} className="bg-black text-white max-w-cardw max-h-cardh m-3 rounded-md">
-          <img src={album.cover} alt={album.title} className="max-w-album max-h-album rounded-md" />
-          <div className="pl-2">
+        <div key={album._id} className="flex flex-col bg-black text-white max-w-cardw max-h-cardh m-3 rounded-md">
+          <div className="flex justify-center py-2">
+            <img src={album.cover} alt={album.title} className="max-w-album max-h-album rounded-md" />
+          </div>
+          <div className="pl-3">
             <h2>{album.name}</h2>
             <h3>{album.artist}</h3>
             <p>{album.price}</p>
-          <div className="flex justify-items-start">
-            {album.genres.map((genre, index) => (
-              <p className="" key={index}>{genre}</p>
-            ))}
-          </div>
+            <div className="flex justify-items-start">
+              {album.genres.map((genre, index) => (
+                <p className="pr-1" key={index}>{genre}</p>
+              ))}
+            </div>
           </div>
         </div>
       ))}
